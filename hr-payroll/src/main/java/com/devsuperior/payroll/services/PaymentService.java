a9ba0f2c0c12e8326c48ada7d490cc3797d258f9
Felipe Ramalho <felipe.ramalho@sicoob.com.br>
2021-07-10 00:15:22 -0300

    hr payroll implementation mock

---
+++ hr-payroll/src/main/java/com/devsuperior/payroll/services/PaymentService.java
@@ -0,0 +1,16 @@
+package com.devsuperior.payroll.services;
+
+import org.springframework.stereotype.Service;
+
+import com.devsuperior.payroll.entities.Payment;
+
+@Service
+public class PaymentService {
+	
+	public Payment getPayment(long workerId, int days) {
+		
+		return new Payment("Bob", 200.0, days);
+		
+	}
+
+}
